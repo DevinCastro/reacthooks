c14c2b3a27f74457ff579a637dd88e562ea0f5db
Devin Castro <dcrocker21@gmail.com>
2020-12-02 15:40:55 -0800

    re created movies app

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [movieState, setMovieState] = useState({
+    movies: [],
+    movie: ''
+  })
+
+  movieState.handleInputChange = event => {
+    setMovieState({ ...movieState, [event.target.name]: event.target.value })
+
+  }
+
+  movieState.handleButtonClick = event => {
+    event.preventDefault()
+
+    axios.get(`http://www.omdbapi.com/?t=${movieState.movie}&apikey=58359cc2`)
+      .then(({ data }) => {
+        console.log(movieState.movies)
+        console.log(data)
+
+        
+       
+        setMovieState({ ...movieState, movies: [data] })
+        
+      })
+      .catch(err => console.log(err))
+  }
+
+
+
+
+  return (
+    <>
+      <form>
+        <label htmlFor="movie">Search for a movie</label>
+        <input
+          type="text"
+          name="movie"
+          value={movieState.movie}
+          onChange={movieState.handleInputChange}
+        />
+        <button onClick={movieState.handleButtonClick}>Submit</button>
+      </form>
+
+      <button onClick={() => console.log(movieState)}>Submit</button>
+
+
+
+
+      <div>
+
+        
+
+
+        {
+            movieState.movies.length > 0 ? (
+              movieState.movies.map(movie => (
+              <ul id="movieList">
+                <li>
+                  <h1>{movie.Title}</h1>
+                  <p>{movie.Plot}</p>
+                </li>
+              </ul>
+              ))
+            ) : null
+          } 
+
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
